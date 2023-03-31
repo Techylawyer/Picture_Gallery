45d6c7b4e6540de3765469b280e410999db71f82
Techylawyer <oluwasegungrace1@gmail.com>
2023-03-31 15:47:52 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+const alternate = document.getElementById("image1");
+
+alternate.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
+
+const alternate2 = document.getElementById("image2");
+
+alternate2.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
+
+const alternate3 = document.getElementById("image3");
+
+alternate3.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
+
+const alternate4 = document.getElementById("image4");
+
+alternate4.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
+
+const alternate5 = document.getElementById("image5");
+
+alternate5.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
+
+const alternate6 = document.getElementById("image6");
+
+alternate6.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
+
+const alternate7 = document.getElementById("image7");
+
+alternate7.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
+
+const alternate8 = document.getElementById("image8");
+
+alternate8.addEventListener("click", (event) => {
+    event.target.style.background = "rgb(80, 1, 113)";
+    event.target.style.color = "rgb(211, 211, 211)";
+});
